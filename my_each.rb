0a1f7011e1adb945c9d2f5a4e54d4217ae556329
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-15 08:41:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,1 @@
-def my_each(array)
-  i = 0
-  new_array = []
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-  if new_array.length <= 0
-    array.map {|name| new_array << "#{name}"}
-    new_array
-  end
-end
+Sky Orbit
