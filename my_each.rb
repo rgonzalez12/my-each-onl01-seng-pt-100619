dbaea25a2a1192db778e3b0e60439388d9e873c5
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-15 01:50:39 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  if new_array.length <= 0
+    array.map {|name| new_array << "#{name}"}
+    new_array
+  end
 end
